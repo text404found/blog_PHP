10c043314f1ad0e4c2c6821bc45e46456deb2382
@void404 <62790695+text404found@users.noreply.github.com>
2024-11-25 11:46:10 -0300

    Update connection.php

--- src/connection.php
+++ src/connection.php
@@ -4,8 +4,13 @@ $user = 'root';
 $password = '';
 $database = 'usersblog';
 
+define('DB_HOST', 'db'); // Nome do serviço no docker-compose
+define('DB_USER', 'blog_user'); // Nome do usuário definido no docker-compose
+define('DB_PASS', 'blog_password'); // Senha definida no docker-compose
+define('DB_NAME', 'blog');
+
 // Conecta ao servidor MySQL
-$conexao = mysqli_connect($host, $user, $password, $database);
+$conexao = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 
 // Verifica se a conexão foi bem-sucedida
 if (!$conexao) {
